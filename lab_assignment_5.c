dae10c2a8f651ecad066b21650277c761cbdc9e1
EpicAsian <epicasianassassin@gmail.com>
2023-06-17 23:15:49 -0400

    Finishing Commit from earlier mistakes

---
+++ lab_assignment_5.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node {
+char letter;
+struct node* next;
+} node;
+// Returns number of nodes in the linkedList.
+int length(node* head)
+{
+    node* current = head;
+
+    int length = 0;
+    while (current!=NULL)
+    {
+        
+        length++;
+        current = current->next;
+    }
+
+    return length;
+}
+// parses the string in the linkedList
+// if the linked list is head -> |a|->|b|->|c|
+// then toCString function wil return "abc"
+char* toCString(node* head)
+{
+    node* current = head;
+    int len = length(head);
+    char* string = (char*)malloc((len+1) * sizeof(char));
+    int i = 0;
+     while(current!=NULL)
+    {
+        string[i]=current->letter;
+        current=current->next;
+        i++;
+    }
+    string[len]='\0';
+    return string;   
+}
+
+// inserts character to the linkedlist
+// f the linked list is head -> |a|->|b|->|c|
+// then insertChar(&head, 'x') will update the linked list as foolows:
+// head -> |a|->|b|->|c|->|x|
+void insertChar(node** pHead, char c)
+{
+    node* new = (node*)malloc(sizeof(node));
+    new->letter = c;
+    new->next = NULL;
+
+    if (*pHead == NULL) {
+        *pHead = new; 
+    }
+    else
+    {
+        node* current = *pHead;
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new;
+    }
+}
+
+
+// deletes all nodes in the linkedList.
+void deleteList(node** pHead)
+{
+    node* current = *pHead;
+    node* next;
+while (current != NULL)
+{
+next = current->next;
+free(current);
+current = next;
+}
+*pHead = NULL;
+}
+int main(void)
+{
+int i, strLen, numInputs;
+node* head = NULL;
+char* str;
+char c;
+FILE* inFile = fopen("input.txt","r");
+fscanf(inFile, " %d\n", &numInputs);
+while (numInputs-- > 0)
+{
+fscanf(inFile, " %d\n", &strLen);
+for (i = 0; i < strLen; i++)
+{
+fscanf(inFile," %c", &c);
+insertChar(&head, c);
+}
+str = toCString(head);
+printf("String is : %s\n", str);
+free(str);
+deleteList(&head);
+if (head != NULL)
+{
+printf("deleteList is not correct!");
+break;
+}
+}
+fclose(inFile);
+}
